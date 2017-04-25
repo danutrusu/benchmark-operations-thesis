bb4d201657a7fa996829bc6d1fa71dbe4f8ed6ce
Danut Rusu <danutmihaitarusu@gmail.com>
2017-04-25 20:20:47 +0200

    revert

--- src/benchmark/DataManager.java
+++ src/benchmark/DataManager.java
@@ -43,7 +43,7 @@ public abstract class DataManager<T> {
      * 
      * @return the time in ms
      */
-    public abstract void generateData() throws Exception;
-//        throw new UnsupportedOperationException("Not supported yet.");
-
+    public void generateData() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
